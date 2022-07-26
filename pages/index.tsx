a6cb7752e518d48ccb4305b33ebc03ce58e5df79
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-07-26 18:04:38 +0000

    Initial Next.js project setup

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>
+          Demix | Find your rice
+        </title>
+      </Head>
+    </>
+  )
+}
+
+export default Home;
