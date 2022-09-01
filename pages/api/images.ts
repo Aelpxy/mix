4b8197b5446343481b6e7c0a0d1074743d921828
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-09-01 16:50:47 +0000

    Feat: new DB schema and API route

---
+++ pages/api/images.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { PrismaClient } from '@prisma/client'
+
+type Data = {
+    statusCode: number
+    message: string,
+    payload: any,
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const db = new PrismaClient();
+
+    if (req.method != "GET") {
+        res.status(405).send({
+            statusCode: 405,
+            message: "Method not allowed",
+            payload: null,
+        })
+    }
+
+    res.status(200).json({
+        statusCode: 405,
+        message: "Method not allowed",
+        payload: await db.image.findMany(),
+    });
+}
