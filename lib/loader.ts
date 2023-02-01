3de2ea7a7957e912537627571e79e60b40fb758c
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2023-02-01 14:11:18 +0000

    style: format

--- lib/loader.ts
+++ lib/loader.ts
@@ -1,8 +1,8 @@
 const loader = ({ src, width, quality }: any) => {
-    if (!quality) {
-        quality = 75
-    }
-    return `https://optimiser.velta.workers.dev?width=${width}&quality=${quality}&image=${src}`
-}
+  if (!quality) {
+    quality = 75;
+  }
+  return `https://optimiser.velta.workers.dev?width=${width}&quality=${quality}&image=${src}`;
+};
 
 export default loader;
